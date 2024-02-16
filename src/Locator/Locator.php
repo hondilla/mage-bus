e38ebc0f54c4e7dee8ec3e0e4b59792d0f16db9a
hondilla <7813599+hondilla@users.noreply.github.com>
2024-02-16 20:19:45 +0100

    drop class locator to external package

--- src/Locator/Locator.php
+++ src/Locator/Locator.php
@@ -2,36 +2,27 @@
 
 namespace Mage\Bus\Locator;
 
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 use ReflectionClass;
 use ReflectionException;
 use ReflectionNamedType;
 use ReflectionParameter;
-use SplFileInfo;
 use function Lambdish\Phunctional\reduce;
 use function Lambdish\Phunctional\search;
 
 abstract class Locator
 {
+    private \Mage\Class\Locator $locator;
+
     public function __construct(
-        private readonly array $paths,
+        array $paths,
         private readonly string $commandInterface,
         private readonly string $commandHandlerInterface
-    ) {}
+    ) {
+        $this->locator = new \Mage\Class\Locator($paths);
+    }
 
     public function mappings(): array
     {
-        /** @psalm-var array<int, SplFileInfo> $files */
-        $files = reduce(
-            /** @psalm-param array<string, string> $path */
-            function (array $acc, array $path): array {
-                return [...$acc, ...$this->searchFiles($path)];
-            },
-            $this->paths,
-            []
-        );
-
         /** @psalm-var array<string, string|array<string>> */
         return reduce(function (array $acc, string $class): array {
             if (class_exists($class) && $this->classImplements($class, $this->commandHandlerInterface)) {
@@ -39,45 +30,7 @@ abstract class Locator
                 $acc = $this->setMapping($class, $acc);
             }
             return $acc;
-        }, $this->filesToClasses($files), []);
-    }
-
-    /** @psalm-param array<string, string> $pathOptions */
-    private function searchFiles(array $pathOptions): array
-    {
-        if (!is_dir($pathOptions['path'])) {
-            return [];
-        }
-
-        /** @psalm-var array<int, SplFileInfo> */
-        return reduce(function (array $acc, SplFileInfo $file) use ($pathOptions): array {
-            if ($file->isFile() && $this->matchFiles($pathOptions['pattern'], $file)) {
-                $acc[] = $file;
-            }
-            return $acc;
-        }, new RecursiveIteratorIterator(new RecursiveDirectoryIterator($pathOptions['path'])), []);
-    }
-
-    private function matchFiles(string $pattern, SplFileInfo $file): bool
-    {
-        /** @infection-ignore-all */
-        if ($pattern === '') {
-            return false;
-        }
-        /** @infection-ignore-all */
-        return preg_match($pattern, $file->getPathname()) === 1;
-    }
-
-    private function filesToClasses(array $files): array
-    {
-        /** @psalm-var array<int, string> */
-        return reduce(function (array $acc, SplFileInfo $file): array {
-            $src = $file->openFile()->fread($file->getSize());
-            if (preg_match('#(namespace)(\\s+)([A-Za-z0-9\\\\]+?)(\\s*);#', $src, $matches)) {
-                $acc[] = $matches[3] . '\\' . $file->getBasename('.php');
-            }
-            return $acc;
-        }, $files, []);
+        }, $this->locator->getClasses(), []);
     }
 
     private function classImplements(string $class, string $implements): bool
